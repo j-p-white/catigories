9d15878bdbe2f426eb5aafa32bc00a9ac6711aff
j-p-white <jwhite8@oswego.edu>
2014-02-04 14:33:42 -0500

    correcty parses files

--- catigories/src/JsoupParser.java
+++ catigories/src/JsoupParser.java
@@ -2,24 +2,25 @@ import java.io.IOException;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 
 public class JsoupParser {
 	
 	public static void main(String [] args){
 		Document doc; 
-		String keyWords;
 		String bodyText;
-		Elements link; 
-		
 		try{ 
-			
 			//get the url
 			doc = Jsoup.connect("http://www.nytimes.com/2014/02/04/science/blazing-trails-in-brain-science.html?ref=science&_r=0").get();
 			
-			
+				// get the body text
+				bodyText = doc.body().text();
+				
+				//replace all not text charecters 
+		String[] myList = bodyText.split("[^a-zA-Z0-9']+");
+				
+			for(int i =0; i < myList.length;i++){
+				System.out.println(myList[i]);
+			}
 			
 		}//end try
 		catch(IOException e){
@@ -27,8 +28,4 @@ public class JsoupParser {
 		}
 		
 	}//end main
-	
-	
-	
-
-}
+}//end class
